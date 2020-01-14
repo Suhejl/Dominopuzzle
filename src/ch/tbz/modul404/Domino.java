4bf04fc499a17b3c2c1c8314770c28035d866ef8
Suhejl Asani <suhejl.asani@edu.tbz.ch>
2020-01-14 16:04:43 +0100

    First Push

---
+++ src/ch/tbz/modul404/Domino.java
@@ -0,0 +1,4 @@
+package ch.tbz.modul404;
+
+public class Domino {
+}
---
+++ src/ch/tbz/modul404/Main.java
@@ -0,0 +1,8 @@
+package ch.tbz.modul404;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
