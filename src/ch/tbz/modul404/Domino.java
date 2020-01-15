c8ba292da909fbb400717462f6f535e5b8319bb0
balkan-King <David.Pavlic0@gmail.com>
2020-01-15 21:57:08 +0100

    pushed project from davids local repo

--- src/ch/tbz/modul404/Domino.java
+++
@@ -1,4 +0,0 @@
-package ch.tbz.modul404;
-
-public class Domino {
-}
--- src/ch/tbz/modul404/Main.java
+++
@@ -1,8 +0,0 @@
-package ch.tbz.modul404;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
